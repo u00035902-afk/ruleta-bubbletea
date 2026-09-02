96a435711012f9cbb407b408ac11beff68b99e52
John <u000302@sise.com>
2026-09-02 17:13:42 -0500

    Agregar migraciones automaticas de SQLite

--- RuletanBublee/Program.cs
+++ RuletanBublee/Program.cs
@@ -43,4 +43,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.Run();
